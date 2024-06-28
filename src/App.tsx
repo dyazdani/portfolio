5e55d10737cb3c53832fd5daff3e875a4c22030c
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-06-28 10:42:21 -0700

    Added grommet theme to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Grommet } from 'grommet';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import theme from './theme';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Grommet theme={theme} full>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Grommet>
   )
 }
 
