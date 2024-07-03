360f5b245425e9b9cc4e56dcf4b3c2472caaaeb0
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-07-03 15:23:04 -0700

    Reformatted placement of social icons

--- src/App.tsx
+++ src/App.tsx
@@ -25,16 +25,17 @@ function App() {
             title="Hi, I'm Dara :-)"
             subtitle="I build full-stack web applications and websites. I am building this portfolio site right now! More coming soon..."
           />
+          <Nav 
+            direction="row" 
+            pad="medium"
+          >
+            <Anchor icon={<Github/>} href='https://github.com/dyazdani'  target='_blank'/>
+            <Anchor icon={<Linkedin/>} href='https://www.linkedin.com/in/darayazdani/'  target='_blank'/>
+            <Anchor icon={<Youtube/>} href='https://youtu.be/_61FuWa92Wo' target='_blank'/>
+          </Nav>
         </PageContent>
       </Page>
-      <Nav 
-        direction="row" 
-        pad="medium"
-      >
-        <Anchor icon={<Github/>} href='https://github.com/dyazdani'  target='_blank'/>
-        <Anchor icon={<Linkedin/>} href='https://www.linkedin.com/in/darayazdani/'  target='_blank'/>
-        <Anchor icon={<Youtube/>} href='https://youtu.be/_61FuWa92Wo' target='_blank'/>
-      </Nav>
+   
     </Main>
     </Grommet>
   )
