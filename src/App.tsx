381162236d6e1fb4b148ce77541227d8cfb61922
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-07-03 07:16:03 -0700

    Added Header and Paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grommet } from 'grommet';
+import { Grommet, Heading, Main, Paragraph } from 'grommet';
 import theme from './theme';
 import CollapsibleNav from './client/components/CollapsibleNav';
 
@@ -7,6 +7,23 @@ function App() {
   return (
     <Grommet theme={theme} full>   
     <CollapsibleNav></CollapsibleNav>
+    <Main>
+      <Heading
+        level={1}
+        textAlign="center"
+        fill
+      >
+        Hi, I'm Dara :-)
+      </Heading>
+      <Paragraph
+        textAlign="center"
+        fill
+        size="large"
+      >
+        I build full-stack web applications and websites.
+      </Paragraph>
+
+    </Main>
     </Grommet>
   )
 }
