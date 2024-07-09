13e5c7692419c8f84f9828be348b204e31f35282
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-07-09 07:47:48 -0700

    Changed base website in Vite config to be HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://www.darayazdani.com/',
+  base: 'https://www.darayazdani.com/',
 })
