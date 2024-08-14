ac2485bdec5103f608c650b6e47e55b21a1c2063
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-08-14 15:39:19 -0700

    Aligned headers and added buttons

--- src/client/components/Projects.tsx
+++ src/client/components/Projects.tsx
@@ -1,13 +1,27 @@
 import { 
     Box,
+    Button,
     Grommet, 
     Heading, 
-    Main
+    Main,
+    ResponsiveContext
 } from "grommet";
 import theme from "../../theme";
 import CollapsibleNav from "./CollapsibleNav";
 
 const Projects = () => {
+
+    const rowButtonBoxStyle = {
+        display: "flex",
+        flexFlow: "row",
+        justifyItems: "center"
+    }
+    const columnButtonBoxStyle = {
+        display: "flex",
+        flexFlow: "column",
+        alignItems: "center"
+    }
+
     return (
         <Grommet 
             theme={theme} 
@@ -17,38 +31,74 @@ const Projects = () => {
             background="#EAD6DF"
             >
                 <CollapsibleNav/>
-                <Heading
-                    level={1}
-                    alignSelf="center"
-                >
-                    Projects
-                </Heading>
                 <Box
-                    alignSelf="center"
-                    width="90vw"
-                    pad={"small"}
-                    responsive
-                >
-                <Heading
-                    level={2}
                     style={{
-                        marginBottom: "0"
+                        display: "flex",
+                        flexFlow: "column",
+                        alignItems: "center"
                     }}
                 >
-                    Trac
-                </Heading>
-                <Heading
-                    level={4} 
-                    fill  
-                    style={{
-                        marginTop: "1rem"
-                    }}                 
-                >
-                    A single-page web application for tracking goals and updating friends on your progress.
-
-                </Heading>
+                    <Heading
+                        level={1}
+                    >
+                        Projects
+                    </Heading>
+                    <Heading
+                        level={2}
+                        margin="0"
+                    >
+                        Trac
+                    </Heading>
+                    <Heading
+                        level={5} 
+                        fill  
+                        margin="0"
+                        style={{
+                            padding: "2rem",
+                            textAlign: "center"
+                        }}
+                    >
+                        A single-page web application for tracking goals and updating friends on your progress.
+                    </Heading>
+                    <ResponsiveContext.Consumer>
+                        {size => (
+                            <Box
+                            style={size === "small" ? columnButtonBoxStyle : rowButtonBoxStyle}
+                            justify="around"
+                        >
+                            <Button 
+                                href="https://trac-kl33.onrender.com" 
+                                label="Visit App"
+                                target="_blank"
+                                margin="1rem"
+                                style={{
+                                    textAlign: "center"
+                                }}
+                            />
+                            <Button 
+                                href="https://youtu.be/_61FuWa92Wo"
+                                label="Watch Demo"
+                                target="_blank"
+                                margin="1rem"
+                                style={{
+                                    textAlign: "center"
+                                }}
+                            />
+                            <Button 
+                                href="https://github.com/dyazdani/trac"
+                                label="View Code"
+                                target="_blank"
+                                margin="1rem"
+                                style={{
+                                    textAlign: "center"
+                                }}
+                            />
+                        </Box>
+                        )}
+                    </ResponsiveContext.Consumer>
+                    
                 </Box>
-
+                
             </Main>
         </Grommet>
     )
