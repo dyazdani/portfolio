8b86976681d817b949dd7de7ce24e0a66211aae1
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-08-16 17:41:21 -0700

    Added social media links to Home.tsx in Nav component  with icons

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,18 @@
 import { 
+  Anchor,
   Box,
   Button,
   Grommet, 
   Heading, 
   Main,
+  Nav,
   ResponsiveContext
 } from 'grommet';
 import theme from './theme';
 import CollapsibleNav from './client/components/CollapsibleNav';
 import Headshot from './client/components/Headshot';
 import { useNavigate } from 'react-router-dom';
+import { Github, Linkedin, Youtube } from 'grommet-icons';
 
 function Home() {
   const navigate = useNavigate();
@@ -37,52 +40,64 @@ function Home() {
         </Heading>
         <ResponsiveContext.Consumer>
           {size => (
-            <Box
-            style={{
-              display: "flex",
-              flexFlow: `${size === "small" ? "column" : "row"}`,
-              alignItems: "center",
-              minHeight: "fit-content",
-              minWidth: "400px"
-            }}
-              justify="around"
-              width="30vw"
-              alignSelf='center'
-              margin="2rem 0"
-          >
-            <Button 
-              label="About"
-              margin="1rem"
-              style={{
-                textAlign: "center"
-              }}
-              onClick={() => {
-                navigate('/about');
-              }}
-            />
-            <Button 
-              label="Projects"
-              margin="1rem"
-              style={{
-                textAlign: "center"
-              }}
-              onClick={() => {
-                navigate('/projects');
-              }}
-            />
-            <Button 
-              href="dara-yazdani-resume.pdf"
-              label="Resume"
-              target='_blank'
-              margin="1rem"
-              style={{
-                textAlign: "center",
-              }}
-            />
-          </Box>
-          )
-
-          }
+            <>
+              <Box
+                style={{
+                  display: "flex",
+                  flexFlow: `${size === "small" ? "column" : "row"}`,
+                  alignItems: "center",
+                  minHeight: "fit-content",
+                  minWidth: "fit-content"
+                }}
+                  justify="around"
+                  width="30vw"
+                  alignSelf='center'
+                  margin="2rem 0"
+              >
+                <Button 
+                  size="large"
+                  label="About"
+                  margin="1rem"
+                  style={{
+                    textAlign: "center"
+                  }}
+                  onClick={() => {
+                    navigate('/about');
+                  }}
+                />
+                <Button 
+                  size="large"
+                  label="Projects"
+                  margin="1rem"
+                  style={{
+                    textAlign: "center"
+                  }}
+                  onClick={() => {
+                    navigate('/projects');
+                  }}
+                />
+                <Button 
+                  size="large"
+                  href="dara-yazdani-resume.pdf"
+                  label="Resume"
+                  target='_blank'
+                  margin="1rem"
+                  style={{
+                    textAlign: "center",
+                  }}
+                />
+              </Box>
+              <Nav 
+                direction="row" 
+                pad="medium"
+                alignSelf='center'
+              >
+                <Anchor icon={<Linkedin size='large'/>} href='https://www.linkedin.com/in/darayazdani/'  target='_blank'/>
+                <Anchor icon={<Github size='large'/>} href='https://github.com/dyazdani'  target='_blank'/>
+                <Anchor icon={<Youtube size='large'/>} href='https://youtu.be/_61FuWa92Wo' target='_blank'/>
+              </Nav>
+            </>
+          )}
         </ResponsiveContext.Consumer>
       </Main>
     </Grommet>
