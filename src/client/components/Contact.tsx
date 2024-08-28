24483782ff927ff88ea27ba084ecbf4b30533e65
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-08-28 09:03:11 -0700

    Added basic contact form that works with EmailJS

--- src/client/components/Contact.tsx
+++ src/client/components/Contact.tsx
@@ -1,8 +1,72 @@
-import { Form } from "grommet";
+import { useState } from "react";
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+
+    // Public key for EmailJS account
+    emailjs.init({
+        publicKey: '8Ob5KiX_qZL4sRtAM' 
+    })
+
+
+    const [name, setName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false)
+;
+    const submit = () => {
+        if (name && userEmail && message) {
+            // Service ID and Template ID from EmailJS account
+            const serviceId = 'service_nw7rhn1'
+            const templateId = 'template_14kmy4s'
+            
+            const templateParams = {
+                name,
+                userEmail,
+                message
+            }
+
+            emailjs.send(serviceId, templateId, templateParams)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setName('');
+            setUserEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
+
     return (
-        <Form/>
+        <div id="contact-form">
+            <input 
+                type="text" 
+                placeholder="Your Name"
+                value={name}
+                onChange={e => setName(e.target.value)} 
+            />
+            <input 
+                type="email" 
+                placeholder="Your email address"
+                value={userEmail}
+                onChange={e => setUserEmail(e.target.value)} 
+            />
+            <textarea 
+                placeholder="Your message"
+                value={message}
+                onChange={e => setMessage(e.target.value)} 
+            />
+            <button 
+                onClick={(e) => {
+                    e.preventDefault();
+                    submit();
+                }}
+            >
+                Send Message</button>
+            {emailSent && <span>Thank you for your message, we will be in touch in no time!</span>}
+        </div>
     )
 }
 
