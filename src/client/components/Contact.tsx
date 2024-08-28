a859d90d7cc3062646f271cf2a026b4c4c6cec2a
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-08-28 10:56:04 -0700

    Added regex function for email validation

--- src/client/components/Contact.tsx
+++ src/client/components/Contact.tsx
@@ -14,6 +14,11 @@ emailjs.init({
     publicKey: '8Ob5KiX_qZL4sRtAM' 
 })
 
+const isValidEmail = (email: string) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(String(email).toLowerCase())
+}
+
 const Contact = () => {
     const [name, setName] = useState('');
     const [userEmail, setUserEmail] = useState('');
